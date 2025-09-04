97be48853868a1927516437df5c9b9b0b517311b
rennanmalmeida <rennanm.almeida@gmail.com>
2025-09-04 18:39:12 +0000

    Atualizar sonar

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -219,7 +219,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const signUp = async (email: string, password: string, fullName: string) => {
     try {
       setLoading(true);
-      const { error, data } = await supabase.auth.signUp({
+      const { error } = await supabase.auth.signUp({
         email,
         password,
         options: {
