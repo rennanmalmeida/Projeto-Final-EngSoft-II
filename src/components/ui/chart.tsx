3839151e7a935f7be1b4aff9d4106389888c4cb5
rennanmalmeida <rennanm.almeida@gmail.com>
2025-09-04 20:10:14 +0000

    Atualizar sonar

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -45,7 +45,14 @@ const ChartContainer = React.forwardRef<
   const chartId = `chart-${id || uniqueId.replace(/:/g, "")}`
 
   return (
-    <ChartContext.Provider value={{ config }}>
+    function Component() {
+  const obj = useMemo(() => ({foo: 'bar'}), []); // value is cached by useMemo
+  return (
+    <SomeContext.Provider value={obj}> { /* Compliant */ }
+      <SomeComponent />
+    </SomeContext.Provider>
+  );
+}
       <div
         data-chart={chartId}
         ref={ref}
