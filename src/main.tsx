8daa36bc5a9e4cc0f73cf7064567c0ee8433ade9
rennanmalmeida <rennanm.almeida@gmail.com>
2025-09-04 18:42:58 +0000

    Atualizar sonar

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot.render(
   <StrictMode>
     <App />
   </StrictMode>,
