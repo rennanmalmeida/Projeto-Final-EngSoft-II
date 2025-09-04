ed65debc1797ced6ed41e02edb7b5b571bd00210
rennanmalmeida <rennanm.almeida@gmail.com>
2025-09-04 20:17:19 +0000

    Atualizar sonar

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -193,17 +193,25 @@ const CarouselItem = React.forwardRef<
   const { orientation } = useCarousel()
 
   return (
-    <div
-      ref={ref}
-      role="group"
-      aria-roledescription="slide"
-      className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
-        className
-      )}
-      {...props}
-    />
+    const CarouselItem = React.forwardRef(
+  ({ className, ...props }, ref) => {
+    const { orientation } = useCarousel()
+
+    return (
+      <div
+        ref={ref}
+        aria-roledescription="slide"
+        className={cn(
+          "min-w-0 shrink-0 grow-0 basis-full",
+          orientation === "horizontal" ? "pl-4" : "pt-4",
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
+CarouselItem.displayName = "CarouselItem"
   )
 })
 CarouselItem.displayName = "CarouselItem"
