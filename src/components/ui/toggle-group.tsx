730c2dfd6d60a937687257e8c2759723006a65f4
rennanmalmeida <rennanm.almeida@gmail.com>
2025-09-04 17:54:54 +0000

    Atualizar sonar

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -22,7 +22,7 @@ const ToggleGroup = React.forwardRef<
     className={cn("flex items-center justify-center gap-1", className)}
     {...props}
   >
-    <ToggleGroupContext.Provider value={{ variant, size }}>
+    <ToggleGroupContext.Provider value={React.useMemo(() => ({ variant, size }), [variant, size])}>
       {children}
     </ToggleGroupContext.Provider>
   </ToggleGroupPrimitive.Root>
